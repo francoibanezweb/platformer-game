f514d57f4181ad59da13d17bcbac57a4bad74e71
francoibanezweb <francoibanez.dev@gmail.com>
2023-09-24 11:41:08 -0300

    updates Frog.gd . tscn

--- Frog.gd
+++ Frog.gd
@@ -10,7 +10,8 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 func _ready():
 	animated_sprite = $AnimatedSprite2D
 	player = $"../../Player/Player"
-
+	animated_sprite.play("Idle")
+	
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	move_and_slide()
@@ -19,11 +20,22 @@ func _on_player_detection_body_entered(body):
 	if body.name == "Player":
 		if body.global_position.x > position.x:
 			animated_sprite.flip_h = true
+			animated_sprite.play("Jump")
 			velocity.x = SPEED
 		else:
 			animated_sprite.flip_h = false
+			animated_sprite.play("Jump")
 			velocity.x = -SPEED
 
 func _on_player_detection_body_exited(body):
 	if body.name == "Player":
+		animated_sprite.play("Idle")
 		velocity.x = 0
+
+
+func _on_player_death_body_entered(body):
+	if body.name == "Player":
+		animated_sprite.play("Death")
+		await animated_sprite.animation_finished
+		self.queue_free()
+
